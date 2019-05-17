437f06788da1fcb6604b9be67e35bb8504cf6efd
Mai Nguyen <mainguyen.vn91@yahoo.com>
2019-05-17 13:54:20 +0200

    part one

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var countryName = process.argv[2];
-var fs = require('fs');
-var fileReader = require('./helper/json-file-reader.js');
+// Part 1:
+const countryname = process.argv[2]
+const fs = require('fs');
 
-fileReader('./resource/countries.json', function (contents) {
-    contents.find(function (country) {
-        if (country.name === countryName) {
+fs.readFile('./resource/countries.json', function (err, contents) {
+    if (err) {
+        throw error
+    };
+    let jsondata = JSON.parse(contents)
+    jsondata.forEach(function (country) {
+        if (country.name === countryname) {
             console.log('Country: ' + country.name)
-            console.log('Top Level Domain: ' + country.topLevelDomain)
+            console.log('Top level domain: ' + country.topLevelDomain)
         }
     })
 })
 
+
+
+// // Part 2:
+// const countryName = process.argv[2];
+// const fs = require('fs');
+// const fileReader = require('./helper/json-file-reader.js');
+
+// fileReader('./resource/countries.json', function (contents) {
+//     contents.find(function (country) {
+//         if (country.name === countryName) {
+//             console.log('Country: ' + country.name)
+//             console.log('Top Level Domain: ' + country.topLevelDomain)
+//         }
+//     })
+// })
--- helper/json-file-reader.js
+++ helper/json-file-reader.js
@@ -1,11 +1,11 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var fileReader = function (filename, callback) {
+let fileReader = function (filename, callback) {
     fs.readFile(filename, 'utf8', function (err, contents) {
         if (err) {
             throw err;
         };
-        var info = JSON.parse(contents);
+        let info = JSON.parse(contents);
         return callback(info)
     });
 }
